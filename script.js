f0f47f6297594c7cff27099bfa9bb8481000be9d
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-07-19 20:09:05 +0530

    all nav part

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-gsap.from("#nav h3",{
+var t1 = gsap.timeline()
+
+t1.from("#nav h3",{
     y:-50,
     opacity:0,
     delay:0.4,
     duration:0.8,
     stagger:0.2
+})
+
+t1.from("#main h1",{
+    x:-500,
+    opacity:0,
+    duration:0.8,
+
 })
